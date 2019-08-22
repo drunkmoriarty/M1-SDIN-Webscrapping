db2341f86a4407710a28df85b59da9bd3316ec14
Maria Rasskazova <45817556+drunkmoriarty@users.noreply.github.com>
2019-08-22 13:52:43 +0200

    Clean code
    Translating the instruction/description from French into English

--- main.py
+++ main.py
@@ -3,35 +3,34 @@ import html
 import re
 import csv
 
-# Modules d'analyse statistique
+#Packages for statistical analysis
 import statistics
 import matplotlib
 import matplotlib.pyplot as mp 
 
-# Modules d'analyse économétrique
+#Packages for econometrics
 import pandas as pd 
 import numpy as np 
 import statsmodels.api as sm 
 import statsmodels.formula.api as smf
 
-#Listes générales
-Prix=[]
-Produits=[]
-#Listes pour les stats
-Moyenne=[]
-Mediane=[]
-Minimum=[]
-Maximum=[]
-Taille_Ech=[]
-#Liste pour l'économétrie
-Red=[]
-Cat_Femme=[]
 
-################# Présentation du programme #######################
+Prix=[] #Will contain all the prices collected from both website
+Produits=[] #Will contain the name of all the product collected
+Moyenne=[] #Means for each category
+Mediane=[] #Median for each category
+Minimum=[] #Minimum for each category
+Maximum=[] #Maximum for each category
+Taille_Ech=[] #Size of the sample
+Red=[] #0 if the product belongs to CafePress // 1 if belongs to Redbubble
+Cat_Femme=[] #0 if it belongs to men's department // 1 if belongs to women's
+
+#The following line presents the goal of this tool : gathering data from two specific website and compare prices for one specific product : tee-shirt 
 print('Ce programme permet de comparer les prix pour les teeshirts sur les marketplaces Redbubble.com et Cafepress.com. \nCette comparaison se fait à l\'aide de statistiques descriptives et d\'un modéle économétrique.')
 
-################################## PARTIE REDBUBBLE ############################################################
-########################### Collecte ###########################################################################
+################################## REDBUBBLE ############################################################
+########################### Collect #####################################################################
+#This section presents to the user the structure of the e-commerce website Redbubble. It tells how many department there are and its links
 print("\n POUR LE SITE REDBUBBLE.COM :")
 url='https://www.redbubble.com/'
 redbubble=requests.get(url)
@@ -41,7 +40,7 @@ redbubble=redbubble.replace('\n','')
 redbubble=redbubble.replace('\t','')
 redbubble=redbubble.replace('\r','')
 
-#Liens relatifs des départements de produits 
+#Gathering name and links for each department
 pattern_red='aria-haspopup="true" href="(.+?(?=">))'
 lien_cat_red=re.findall(pattern_red, redbubble)
 
@@ -50,13 +49,16 @@ nom_cat_red=re.findall(pattern_red_name, redbubble)
 del(nom_cat_red[4]) 
 del(nom_cat_red[4])
 del(nom_cat_red[8])
+
+#Announcing the permanent structure
 print('Le Site RedBubble présente ', len(nom_cat_red),' départements de produits.')
 print(nom_cat_red)
+#Announcing the choosen departments for the scrapping
 print("Dans le cadre de cette analyse, les départements de produits ciblés sont : ", nom_cat_red[0], "et", nom_cat_red[1])
 
+#Gathering the name for each product presented in both department
 Gamme_red=[]
 Lien_gamme_red=[]
-############# GENERER UNE LISTE POUR CHAQUE DEPARTEMENTS CIBLE AVEC LES GAMMES PRESENTES #############
 for k in range(0,2):
     url = "https://www.redbubble.com"
     url = requests.compat.urljoin(url,lien_cat_red[k])
@@ -74,21 +76,21 @@ for k in range(0,2):
     pattern_lien_red_cat='<a tabindex="0" class="Cards__link--1iiMg" href="(.+?(?="><div class="))'
     red_gamme_lien=re.findall(pattern_lien_red_cat,cat)
     Lien_gamme_red.extend(red_gamme_lien)
+    #Announcing the user the product for whose price will be analysed
     print ("Il conviendra de se concentrer sur la première gamme :",red_gamme[0])
 
-########### ETUDE 1ERE GAMME DE CHAQUE DEPARTEMENT --> TEE SHIRT ##################
-T_Red=[] #Gamme T-shirt de chaque département
-T_Red_Lien=[] #Lien de la gamme de T-shirt pour chaque département
-
+# Gathering data (price and name of the products) from Tee-shirt 
+T_Red=[] 
+T_Red_Lien=[] 
 T_Red.append(Gamme_red[0])
 T_Red.append(Gamme_red[3])
 T_Red_Lien.append(Lien_gamme_red[0])
 T_Red_Lien.append(Lien_gamme_red[3])
 
-Red_Prix=[]
+Red_Prix=[] #Lists of all the price collected from Redbubble
 
-Prix_R_H=[]
-Prix_R_F=[]
+Prix_R_H=[] #Prices of Tee_shirts from Redbubble men's department
+Prix_R_F=[] #Prices of Tee_shirts from Redbubble women's department
 for i in range(0,2):
     print("\nPour le département ", nom_cat_red[i], ':')
     Prix_Red=[]
@@ -105,7 +107,6 @@ for i in range(0,2):
         gam=gam.replace('\t','_')
         gam=gam.replace('&#39;',' ')
         
-        ###### RECUPERER LISTE DE PRODUITS ET LISTE DES PRIX  #################################################
         pat_produits='<h2 class="default__text--3DN1l default__caption--3cJd6 default__muted--1ydZD styles__fullTitle--2XH1d" element="h2">(.+?(?=</h2>))'
         produit_red=re.findall(pat_produits, gam)
         Produit_Red.extend(produit_red)
@@ -132,7 +133,7 @@ for i in range(0,2):
             Cat_Femme.append(i)
         Prix_R_F.extend(Prix_Red)
 
-    ############################ STATISTIQUES DESCRIPTIVES ################################################
+    #Stats 
     print("Les statistiques descriptives se ferront sur la base de ", taille, " produits présentés sur la premières page.")
     moyenne=statistics.harmonic_mean(Prix_Red)
     print("Le prix moyen d'un produit de cette gamme est de ", moyenne,"€.")
@@ -151,18 +152,14 @@ for i in range(0,2):
     print("Ce prix correspond au produit ", Produit_Red[f])
     Minimum.append(minimum)
 
-#Rapide mise au point econometrique#
+#Econometrics
 var=Prix_R_H+Prix_R_F
 for k in range(len(var)):
     Red.append(0)
 
-###################################################################################################################
-##################################################################################################################
-###################################################################################################################
 ###################################### CAFEPRESS.COM #############################################################
 
 print('\n \n \n POUR LE SITE CAFEPRESS :')
-
 url='https://www.cafepress.com/'
 main=requests.get(url)
 main=main.text
@@ -171,27 +168,24 @@ main=main.replace('\n','')
 main=main.replace('\t','')
 main=main.replace('\r','')
 
-#################################### RECUPERER L ENSEMBLE DES DEPARTEMENTS ####################################
+# Gathering the name of all the departments of the webstore
 caf_cat='<a class="parent-link" reset-ui="" href="//www.cafepress.com/\+(.+?(?=">))'
 caf_cat=re.findall(caf_cat, main)
 
 print("Le site Cafe Press présente ", len(caf_cat), " départements de produits.")
 print(caf_cat)
 print('Dans le cadre de notre analyse, les départements ciblés sont :', caf_cat[0], "et", caf_cat[1])
-
-#Liens relatifs des départements de produits
 caf_lien='<a class="parent-link" reset-ui="" href="//www.cafepress.com/(.+?(?=">))'
 caf_lien=re.findall(caf_lien, main)
 
-################################## ETUDE GAMME DE T-SHIRTS DE POUR CHACUN DES DEUX DEPARTEMENTS ################
+#This part differs from the one used for Redbubble. The website structure is different and doesn't allow
 caf_cat=[w.replace('-clothing', '-t-shirts') for w in caf_cat] 
 caf_lien=[y.replace('-clothing', '-t-shirts') for y in caf_lien]
-#^ Bon j'avoue, j'ai eu la flemme d'automatiser la recherche du lien pour la gamme de tee-shirt
 
-Caf=[]
-Caf_Prix=[]
-Prix_C_H=[]
-Prix_C_F=[]
+Caf=[] #List containing the name of the product
+Caf_Prix=[] #List containing the price
+Prix_C_H=[] #Price for tee-shirts in the men's department
+Prix_C_F=[] #Price for tee-shirst in women's department
 
 for z in range(0,2):
     print('\nPour les T-shirts du département ', caf_cat[z],':')
@@ -253,32 +247,13 @@ for z in range(0,2):
     """print("Ce prix correspond au produit ", Produitc[f])"""
     Minimum.append(minimum)
 
-#Mise au point Econometrie
 var=Prix_C_H+Prix_C_F
 for k in range(len(var)):
     Red.append(1)
 
 
-################################## GRAPHIQUE - HISTOGRAMME & STATISTIQUES DESCRIPTIVES  ############################################
-'''
-x1=Prix_R_H
-x2=Prix_R_F
-mp.hist([x1,x2], range=(15, 41), bins=5, density=1, color=['blue', 'pink'], edgecolor='xkcd:black', label=['Dep.Homme','Dep.Femme'])
-mp.title('Répartition des prix sur le site Redbubble', fontdict=None, loc='center', pad=None)
-mp.xlabel('Prix en euros')
-mp.ylabel('Pourcentage de répartition')
-mp.legend()
-mp.show()
-
-x1=Prix_C_H
-x2=Prix_C_F
-mp.hist([x1,x2], range=(15, 41), bins=5, density=1, color=['blue', 'pink'], edgecolor='xkcd:black', label=['Dep.Homme','Dep.Femme'])
-mp.title('Répartition des prix sur le site Cafepress', fontdict=None, loc='center', pad=None)
-mp.xlabel('Prix en euros')
-mp.ylabel('Pourcentage de répartition')
-mp.legend()
-mp.show()
-'''
+#Graphs
+#The first graph compares the price for men's tee-shirt for both webstores
 x1=Prix_R_H
 x2=Prix_C_H
 mp.hist([x1,x2], range=(15, 41), bins=5, density=1, color=['red', 'green'], edgecolor='xkcd:black', label=['Redbubble','CafePress'])
@@ -288,6 +263,7 @@ mp.ylabel('Pourcentage de répartition')
 mp.legend()
 mp.show()
 
+#The second graph compares the price for men's tee-shirt for both webstores
 x1=Prix_R_F
 x2=Prix_C_F
 mp.hist([x1,x2], range=(15, 41), bins=5, density=1, color=['red', 'green'], edgecolor='xkcd:black', label=['Redbubble','CafePress'])
@@ -297,22 +273,22 @@ mp.ylabel('Pourcentage de répartition')
 mp.legend()
 mp.show()
 
-
+#The following lines creates a CSV file with all the stats for each department and each store
 with open("Statistiques descriptives des deux sites.csv", 'w', encoding='utf-8')as outfile :
     data=csv.writer(outfile, delimiter='\t', lineterminator='\n')
     data.writerow(['Taille Echantillon', 'Prix moyen', 'Prix médian', 'Prix minimal', 'Prix maximal'])
     data.writerows(zip(Taille_Ech, Moyenne, Mediane, Minimum, Maximum))
 
-########################################################################################################################
-########################################################################################################################
-######################################### ECONOMETRIE #################################################################
+#Econometrics
 print('\n Il conviendra d\'estimer le prix d\'un tee-shirt en fonction de son appartenance à un site, à une catégorie.')
 print('La référence est l\'appartenance du produit au site redbubble et au département homme.')
+#This allows to create a CSV data base with all the name, price and website
 with open("base.csv", 'w', encoding='utf-8')as base:
     data=csv.writer(base, lineterminator='\n')
     data.writerow(['Prix', 'Site', 'Femme'])
     data.writerows(zip(Prix, Red, Cat_Femme))
 
+#This part is to show the user the regression made based on the database
 base=pd.read_csv('base.csv')
 reg=smf.ols('Prix ~ Site + Femme', data=base)
 results=reg.fit()
